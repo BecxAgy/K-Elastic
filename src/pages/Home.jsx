8c85fd3be19de8729d08d685e82e67c611b25c17
rebeca <beca.cout@gmail.com>
2023-10-04 13:58:57 -0300

    chore: solve issue related grid card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import SearchNav from '../components/SearchNav'
+import CardDocument from '../components/CardDocument'
 
 function Home() {
-    return <div>Home</div>
+    return (
+        <div className='grid grid-flow-row gap-6'>
+            <SearchNav />
+            <div className='grid grid-cols-4 gap-10 '>
+                <CardDocument />
+                <CardDocument />
+                <CardDocument />
+                <CardDocument />
+                <CardDocument />
+                <CardDocument />
+                <CardDocument />
+                <CardDocument />
+                <CardDocument />
+                <CardDocument />
+                <CardDocument />
+                <CardDocument />
+            </div>
+        </div>
+    )
 }
 
 export default Home
